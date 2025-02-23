aca83f282387371b5f0047529b25f129b60a53dc
mrohling13 <malachi.rohling@yahoo.com>
2025-02-23 04:00:31 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -21,7 +21,7 @@ class Product {
     }
 }
 
-// Test cases for Task 1
+// Test Cases for Task 1
 
 const prod1 = new Product("Laptop", 101, 1200, 10);
 console.log(prod1.getDetails());
@@ -29,3 +29,24 @@ console.log(prod1.getDetails());
 prod1.updateStock(3);
 console.log(prod1.getDetails());
 
+// Task 2: Creating an Order Class
+
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        this.totalPrice = product.price * quantity; // Calculates Total Price
+        product.updateStock(quantity);
+    }
+
+    getOrderDetails() {
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.totalPrice}`;
+    }
+}
+
+// Test Cases for Task 2
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails());
+
+console.log(prod1.getDetails());
